25ee975ece049b6b7dce422c15dc2798126dc1dc
IharRubanik <74908305+IharRubanik@users.noreply.github.com>
2021-03-03 16:33:16 +0300

    fix: class key click

--- main.js
+++ main.js
@@ -1,39 +1,25 @@
-// создали переменную keys и положили в неё все клавиши
 let keys = document.querySelectorAll('.key');
-// повесили на весь маассив обработчик событий, после запустили функцию playNote
-keys.forEach(key => {
- key.addEventListener('click',  playNote);
+  
+    keys.forEach(key => {
+    key.addEventListener('click',  playNote);
 });
-// создали функию PlayNote, 
+    document.addEventListener('keydown',  playNote);
+
 function playNote(e) {
-    // будем помещать в "е" событие которое поймали
- let key = e.target;
-// так как атрибут data-note совподает с id аудио тегов, получаем его следующим образом
- let note = document.getElementById(key.dataset.note);
-//  добавили класс active при клике
- key.classList.add('active');
-// для короткого звучания добавили currentTime
- note.currentTime = 0;
- note.play();
-//  удаляем класс Active при окончании звучания мелодии
+    let key = e.keyCode || e.target.dataset.note,
+        note = document.getElementById(key),
+        keyNote = document.querySelector('[data-note="'+key+'"]');
+
+    keyNote.classList.add('active');
+    note.play();
+    note.currentTime = 0;
+
  note.addEventListener('ended',() => {
-  key.classList.remove('active');
+  keyNote.classList.remove('active');
  });
 
 }
-// повесили обработчик событий на весь докуент при нажатии клавиш
-document.addEventListener('keydown',  playNoteKey);
-// создали функцию, которая принимает событие клавиатуры
-   function playNoteKey (i) {
-    // создали переменную, которая равна букве клавиатуры 
-    let kDown = i.key;
-        let audio = document.getElementById(kDown);
-        audio.currentTime = 0;
-        audio.play();
-        
-        
 
-   }
 
 
 
---
+++ script.js
@@ -0,0 +1,11 @@
+let array = document.querySelectorAll('.key');
+console.log(keys);
+
+keys.addEventListener('click',  playNote);
+
+
+
+
+
+
+//    document.addEventListener('keydown',  playNoteKey);
